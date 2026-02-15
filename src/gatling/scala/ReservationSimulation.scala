e8d164b26b282352e13614b10660f701bdf3fccd
Rasool Malekinejad <rasool.malekinejad@gmail.com>
2026-02-15 14:43:07 +0330

    Reduce TPS because the target app is running on a limited hardware

--- src/gatling/scala/ReservationSimulation.scala
+++ src/gatling/scala/ReservationSimulation.scala
@@ -6,10 +6,10 @@ import scala.util.Random
 class ReservationSimulation extends Simulation {
 
   private val tps: Double =
-    sys.props.get("TPS").map(_.toDouble).getOrElse(1000)
+    sys.props.get("TPS").map(_.toDouble).getOrElse(500)
 
   private val durationSeconds: Int =
-    sys.props.get("DURATION").map(_.toInt).getOrElse(900)
+    sys.props.get("DURATION").map(_.toInt).getOrElse(1000)
 
   private val httpProtocol = http
     .baseUrl("http://localhost:8080")
